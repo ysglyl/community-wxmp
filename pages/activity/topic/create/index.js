e28f97626de9c2d074ddadc08dabc67a2b75582d
yushigui <yushigui1990@foxmail.com>
2020-03-19 20:49:28 +0800

    feature

--- pages/activity/topic/create/index.js
+++ pages/activity/topic/create/index.js
@@ -1,66 +1,130 @@
-// pages/activity/topic/create/index.js
+// pages/activity/notice/create/index.js
+import {
+  post
+} from '../../../../utils/util.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    communityType: [{
+      rowId: 0,
+      name: '社区'
+    }, {
+      rowId: 1,
+      name: '家园'
+    }, {
+      rowId: 2,
+      name: '团体'
+    }],
+    communitys: [],
+    selectedCommunity: [0, 0]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    this.loadAllCommunity();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-  }
+  },
+  bindChangeType: function(e) {
+    if (e.detail.column === 0) {
+      this.setData({
+        selectedCommunity: [e.detail.value, 0]
+      });
+    }
+  },
+  bindSelectCommunity: function(e) {
+    if (this.data.communitys[this.data.selectedCommunity[0]].length === 0) {
+      return;
+    }
+    this.setData({
+      selectedCommunity: e.detail.value
+    })
+  },
+  loadAllCommunity: function() {
+    const $this = this;
+    post({
+      url: 'community/myAllList',
+      success: function(res) {
+        $this.setData({
+          communitys: [res.filter(item => item.type === 0), res.filter(item => item.type === 1), res.filter(item => item.type === 2)]
+        })
+      }
+    })
+  },
+  bindFormSubmit: function(e) {
+    const $this = this;
+    const community = this.data.communitys[this.data.selectedCommunity[0]][this.data.selectedCommunity[1]] == undefined
+    if (community == undefined) {
+      wx.showToast({
+        title: '请选择社区',
+        icon: 'none'
+      })
+      return;
+    }
+    post({
+      url: 'activity/insert',
+      data: {
+        type: 2,
+        communityId:community.rowId,
+        topic: {
+          ...e.detail.value
+        }
+      },
+      success: function(res) {
+        wx.navigateBack({})
+      }
+    })
+  },
 })
